b1a485ec1e60dd0b49ba8a18ecdc563518b37402
ningyin <yinning@pku.edu.cn>
2015-06-11 17:59:29 +0800

    2nd

--- src/utillity/Utillity.java
+++ src/utillity/Utillity.java
@@ -1,7 +1,9 @@
+package utillity;
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 
 public class Utillity {
@@ -12,6 +14,10 @@ public class Utillity {
 		
 		try {
 			Document document = Jsoup.connect(url).get();
+//			System.out.println(document.toString());
+			Elements es=document.getElementsByClass("journal-item");
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
